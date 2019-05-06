cbba734732502b6b7ec61729bf2789d566dc77c9
Pablo Varela <pablo.varela@bgch.co.uk>
2019-05-06 14:53:14 +0100

    asdf

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run moonboard app')
     parser.add_argument('--debug', action="store_true", default=False, help='Debug modus')
-    parser.add_argument('--host', action="store", default='0.0.0.0:wq', help='Host addressw')
+    parser.add_argument('--host', action="store", default='0.0.0.0', help='Host addressw')
     parser.add_argument('--port', type=int, default=5000, help='Server port')
     args = parser.parse_args()
 
